5275f8e063ba15b8849a736567eacb9acc029dbe
javmunmay <javi15mmj@gmail.com>
2025-02-12 18:21:39 +0100

    carrusel-imagenes carrusel hecho, falta el vídeo

--- src/app/components/carrusel-imagenes/carrusel-imagenes.component.ts
+++ src/app/components/carrusel-imagenes/carrusel-imagenes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carrusel-imagenes',
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './carrusel-imagenes.component.css'
 })
 export class CarruselImagenesComponent {
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.startImageSlider();
+  }
+
+  startImageSlider(): void {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    function changeSlide() {
+      // Quitar la clase 'active' de la imagen actual
+      slides[currentSlide]?.classList.remove('active');
+
+      // Generar un índice aleatorio para la siguiente imagen
+      do {
+        currentSlide = Math.floor(Math.random() * slides.length);
+      } while (slides[currentSlide]?.classList.contains('active')); // Evitar repetir la misma imagen
+
+      // Agregar la clase 'active' a la nueva imagen
+      slides[currentSlide]?.classList.add('active');
+    }
+
+    // Cambiar la imagen cada 2 segundos
+    setInterval(changeSlide, 2000);
+  }
 
 }
