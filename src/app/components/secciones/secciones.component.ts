f7a9f3e0171658130e316f47231c471adae000e1
Ferbarmor <ferbarmor@gmail.com>
2025-02-12 13:51:20 +0100

    Primera subida con secciones Fernando

---
+++ src/app/components/secciones/secciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secciones',
+  standalone: false,
+  templateUrl: './secciones.component.html',
+  styleUrl: './secciones.component.css'
+})
+export class SeccionesComponent {
+
+}
