ae52b42b0553c0a48607b17c07aba88b7decf58f
Leandro Ligero <145332108+RLeandro05@users.noreply.github.com>
2025-02-16 15:25:33 +0100

    Añadido componente de habitosSaludables por hacer y cambios en informacionSalud

---
+++ src/app/components/habitos-saludables/habitos-saludables.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habitos-saludables',
+  standalone: false,
+  templateUrl: './habitos-saludables.component.html',
+  styleUrl: './habitos-saludables.component.css'
+})
+export class HabitosSaludablesComponent {
+
+}
