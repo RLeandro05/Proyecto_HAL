a81643fe54a89790e31c645d8ca39ed487675fd7
javmunmay <javi15mmj@gmail.com>
2025-02-16 15:17:09 +0100

    carrusel increible hecho, falta perfeccionar detalles

--- src/app/components/carrusel-imagenes/carrusel-imagenes.component.ts
+++ src/app/components/carrusel-imagenes/carrusel-imagenes.component.ts
@@ -8,32 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarruselImagenesComponent {
 
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.startImageSlider();
-  }
-
-  startImageSlider(): void {
-    const slides = document.querySelectorAll('.slide');
-    let currentSlide = 0;
-
-    function changeSlide() {
-      // Quitar la clase 'active' de la imagen actual
-      slides[currentSlide]?.classList.remove('active');
-
-      // Generar un índice aleatorio para la siguiente imagen
-      do {
-        currentSlide = Math.floor(Math.random() * slides.length);
-      } while (slides[currentSlide]?.classList.contains('active')); // Evitar repetir la misma imagen
-
-      // Agregar la clase 'active' a la nueva imagen
-      slides[currentSlide]?.classList.add('active');
+  nextSlide() {
+    const items = document.querySelectorAll('.item');
+    const slideContainer = document.querySelector('.slide');
+    if (slideContainer) {
+      slideContainer.appendChild(items[0] as Node);
     }
-
-    // Cambiar la imagen cada 2 segundos
-    setInterval(changeSlide, 2000);
   }
+
+  prevSlide() {
+    const items = document.querySelectorAll('.item');
+    const slideContainer = document.querySelector('.slide');
+    if (slideContainer && items.length > 0) {
+      slideContainer.prepend(items[items.length - 1] as Node);
+    }
+  }
+
 
 }
