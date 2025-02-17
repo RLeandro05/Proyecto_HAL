9ec1e06bbbfbf2fa80076d750dfe673962217a61
Ferbarmor <ferbarmor@gmail.com>
2025-02-17 14:52:55 +0100

    Componente estilos vida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { GraficosComponent } from './components/graficos/graficos.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HabitosInsalubresComponent } from './components/habitos-insalubres/habitos-insalubres.component';
 import { HabitosSaludablesComponent } from './components/habitos-saludables/habitos-saludables.component';
+import { EstilosVidaComponent } from './components/estilos-vida/estilos-vida.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { HabitosSaludablesComponent } from './components/habitos-saludables/habi
     QuizComponent,
     HabitosInsalubresComponent,
     HabitosSaludablesComponent,
+    EstilosVidaComponent,
     
   ],
   imports: [
