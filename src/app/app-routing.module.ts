99bb9f111e42b83d11d0392a4ac3a09f3ea76509
mcuerod <mcuerod1910@g.educaand.es>
2025-02-19 14:51:18 +0100

    sobrenosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { GraficosComponent } from './components/graficos/graficos.component';
 import { QuizComponent } from './components/quiz/quiz.component';
 import { HabitosInsalubresComponent } from './components/habitos-insalubres/habitos-insalubres.component';
 import { HabitosSaludablesComponent } from './components/habitos-saludables/habitos-saludables.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path:"",component: HomeComponent},
   {path:"graficos",component: GraficosComponent},
   {path:"quiz",component: QuizComponent},
   {path:"hInsalubres",component: HabitosInsalubresComponent},
-  {path: "hSaludables", component: HabitosSaludablesComponent}
+  {path: "hSaludables", component: HabitosSaludablesComponent},
+  {path: "sobreNosotros", component: AboutComponent}
 
 ];
 
